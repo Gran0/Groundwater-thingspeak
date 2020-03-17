48d7aaae8e3bb797b6b2695a6d992f34e96d33e3
Lukáš Granzer <gran0lukas@gmail.com>
2020-03-17 12:29:47 +0100

    Start
    Inicialization of project

---
+++ Groundwater-thingspeak/Groundwater-thingspeak.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		Groundwater_thingspeak.ino
+ Created:	17.03.2020 11:59:51
+ Author:	Lukas Granzer
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(9600);
+	Serial.write("Epstein didnt kill himself.");
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	;;
+}
